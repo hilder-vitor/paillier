4a45032a777d61a129f856bc1f88793cfceb7cd8
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2017-12-01 13:37:35 +0100

    Improved tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include"Paillier.h"
 #include<iostream>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 template <typename ELEMENT>
 void operator+=(vector<ELEMENT>& u, const vector<ELEMENT>& v){
@@ -29,13 +31,51 @@ vector<mpz_class> mult_component_wise(vector<mpz_class> u, vector<mpz_class> v){
 }
 
 int main(){
-	paillier::Paillier p(2048);
+	paillier::Paillier p(1024);
 
-	mpz_class m1 = 50;
-	mpz_class m2 = 11231233;
+	mpz_class m1 = 501;
+	mpz_class m2 = 112312313;
 
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 	paillier::Ciphertext c1 = p.enc(m1);
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	auto duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Encryption time for m1: " << duration << endl;
+	
+	t1 = high_resolution_clock::now();
 	paillier::Ciphertext c2 = p.enc(m2);
+	t2 = high_resolution_clock::now();
+	duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Encryption time for m1: " << duration << endl;
+
+	t1 = high_resolution_clock::now();
+	paillier::Ciphertext c_add_1 = p.add(c1,c2);
+	t2 = high_resolution_clock::now();
+	duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Running time homomorphic m1 + m2: " << duration << endl;
+
+	t1 = high_resolution_clock::now();
+	paillier::Ciphertext c_add_2 = p.add(c2,c1);
+	t2 = high_resolution_clock::now();
+	duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Running time homomorphic m2 + m1: " << duration << endl;
+
+	t1 = high_resolution_clock::now();
+	paillier::Ciphertext c_prod_1 = p.mul(c1, m2);
+	t2 = high_resolution_clock::now();
+	duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Running time homomorphic c1*m2: " << duration << endl;
+
+	t1 = high_resolution_clock::now();
+	paillier::Ciphertext c_prod_2 = p.mul(c2, m1);
+	t2 = high_resolution_clock::now();
+	duration = duration_cast<microseconds>( t2 - t1 ).count() * 0.001;
+	cout << "Running time homomorphic c21*m1: " << duration << endl;
+
+
+
+
+
 
 	if (p.dec(c1) == m1)
 		cout << "ENC(" << m1 << ") .....  OK" << endl;
@@ -47,21 +87,21 @@ int main(){
 	else
 		cout << "ENC(" << m2 << ") .....  ERROR" << endl;
 
-	if (p.dec(p.add(c1, c2)) == m1+m2)
+	if (p.dec(c_add_1) == m1+m2)
 		cout << "ENC(" << m1 << " + " << m2 << ") .....  OK" << endl;
 	else
 		cout << "ENC(" << m1 << " + " << m2 << ") .....  ERROR" << endl;
 
-	if (p.dec(p.add(c2, c1)) == m2+m1)
+	if (p.dec(c_add_2) == m2+m1)
 		cout << "ENC(" << m2 << " + " << m1 << ") .....  OK" << endl;
 	else
 		cout << "ENC(" << m2 << " + " << m1 << ") .....  ERROR" << endl;
 
-	if (p.dec(p.mul(c1, m2)) == m1*m2)
+	if (p.dec(c_prod_1) == m1*m2)
 		cout << "ENC(" << m1 << " * " << m2 << ") .....  OK" << endl;
 	else
 		cout << "ENC(" << m1 << " * " << m2 << ") .....  ERROR" << endl;
-	if (p.dec(p.mul(c2, m1)) == m1*m2)
+	if (p.dec(c_prod_2) == m1*m2)
 		cout << "ENC(" << m2 << " * " << m1 << ") .....  OK" << endl;
 	else
 		cout << "ENC(" << m2 << " * " << m1 << ") .....  ERROR" << endl;
